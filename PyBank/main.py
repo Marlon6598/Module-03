e0c15ab335062dd9e8f49247853e5bbced390cb0
Marlon6598 <marlon6598@gmail.com>
2023-02-10 22:34:39 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,12 +6,21 @@ csvFilePath = os.path.join('Resources', 'budget_data.csv') # Specifies directory
 with open(csvFilePath, "r", encoding="utf-8") as csvFile: # opens the file and saves it as an object named csvFile
     csvReader = csv.reader(csvFile, delimiter=",") #specifies the object name and delimiter for the reader in the open() function
     header = next(csvReader) # reads the row of headers and skips over this row for our data
-    
+    firstRow = next(csvReader)
     totalMonths = len(list(csvReader)) # the total months is the length of the dataset minus the header
+    prevChange = int(firstRow[1])
 
-profits = [] # creates an empty variable to contain the data from the profits & losses column in the csv file
-for row in csvReader:
-        profits.append(row[1])
+    # dates = [] # creates an empty variable to contain the data from the date column in the csv file
+    profits = [1] # creates an empty variable to contain the data from the profits/losses column in the csv file
+    rowCount = 0
+
+    for row in csvReader:
+        change = int(row[1]) - prevChange
+        profits.append(change)
+        prevChange = int(row[1])
+        rowCount = rowCount + 1
+
+average = sum(profits)/(len(profits))
 
 
 
@@ -20,6 +29,6 @@ print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {totalMonths}")
 print(f"Total: ${[]}")
-print(f"Average Change: ")
+print(f"Average Change: {average}")
 print(f"Greatest increase in Profits: ")
 print(f"Greatest Decrease in Profits: ")
