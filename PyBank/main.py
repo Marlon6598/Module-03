4047fea4e16de60cd430ec1af775b926f29ee0a0
Marlon6598 <marlon6598@gmail.com>
2023-02-10 22:13:03 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,20 +3,23 @@ import csv # imports file-handling funcions
 
 csvFilePath = os.path.join('Resources', 'budget_data.csv') # Specifies directory of .csv file
 
-with open(csvFilePath) as csvFile: # opens the file and saves it as an object named csvFile
+with open(csvFilePath, "r", encoding="utf-8") as csvFile: # opens the file and saves it as an object named csvFile
     csvReader = csv.reader(csvFile, delimiter=",") #specifies the object name and delimiter for the reader in the open() function
     header = next(csvReader) # reads the row of headers and skips over this row for our data
     
     totalMonths = len(list(csvReader)) # the total months is the length of the dataset minus the header
 
-    totalProfit = 1
+profits = [] # creates an empty variable to contain the data from the profits & losses column in the csv file
+for row in csvReader:
+        profits.append(row[1])
+
 
 
 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {totalMonths}")
-print(f"Total: ${totalProfit}")
+print(f"Total: ${[]}")
 print(f"Average Change: ")
 print(f"Greatest increase in Profits: ")
 print(f"Greatest Decrease in Profits: ")
